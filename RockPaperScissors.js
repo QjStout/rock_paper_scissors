12355f30a362a6c1396335bd8921f2cd06d63a04
Qj Stout <qjstoutspykers@gmail.com>
2021-07-02 19:05:26 +1000

    Add function to normalise player inputs

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,6 +1,14 @@
+function normaliseInput(playerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    return playerSelection
+        .charAt(0)
+        .toUpperCase()
+        + playerSelection.slice(1);
+}
+
 function computerPlay() {
     const hand = ['Rock', 'Paper', 'Scissors'];
     const rand = Math.floor(Math.random() * hand.length);
     return hand[rand];
 }
-
